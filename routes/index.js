a66e8424ea1613c4fd40b09bb41998b1ee6d2e83
Paul Potsides <paul@koble.ai>
2023-09-03 12:56:53 +0200

    Tooltips and .env

--- routes/index.js
+++ routes/index.js
@@ -1,32 +1,36 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+require('dotenv').config()
+const awskey = process.env.AWS_ACCESS_KEY_ID || '';
+const awssecretkey = process.env.AWS_SECRET_ACCESS_KEY || '';
+const awsregion = process.env.AWS_REGION || 'us-east-1';
 
-var awskey = process.env.AWS_ACCESS_KEY_ID || '';
-var awssecretkey = process.env.AWS_SECRET_ACCESS_KEY || '';
-var awsregion = process.env.AWS_REGION || 'us-east-1';
-var AWS = require('aws-sdk');
-AWS.config.update({accessKeyId: awskey, secretAccessKey: awssecretkey, region: awsregion});
-var s3 = new AWS.S3();
+const bucketIds = JSON.parse(process.env.BUCKETS || '[]');
+const fakeBucket = 'FakeBucket';
+const AWS = require('aws-sdk');
+if(awskey && awssecretkey && awsregion) {
+  AWS.config.update({accessKeyId: awskey, secretAccessKey: awssecretkey, region: awsregion});
+}
+const s3 = new AWS.S3();
 
-//var IMAGE_TYPEFILTER = process.env.IMAGE_TYPEFILTER || '.png,.jpg';
+const IMAGE_EXTENSIONS = new Set(JSON.parse(process.env.IMAGE_EXTENSIONS || '["apng","avif","gif","jpg","jpeg","jfif","pjpeg","pjp","png","svg","webp"]'));
 
 //----------------------------------------------------------------------------
 // validate the images and filter them according to prefs
 //----------------------------------------------------------------------------
 function filterImages(data) {
-    // TODO filter based on types configured (case insensitive)
-    return data;
+    return data.filter(img => IMAGE_EXTENSIONS.has(img.split('.').pop()));
 }
 
 //----------------------------------------------------------------------------
 // loop through S3 formatted API results and build an images list
 //----------------------------------------------------------------------------
 function buildImagesListFromS3Data(bucketname, data) {
-    const S3_PREFIX = 'https://s3.amazonaws.com/' + bucketname + '/';
-    var images = [];
-    var contents = data.Contents
+    const S3_PREFIX = `https://${bucketname}.s3.${awsregion}.amazonaws.com/`;
+    const images = [];
+    const contents = data.Contents
     //console.log("iterating " + JSON.stringify(contents));
-    for (var iter in contents) {
+    for (const iter in contents) {
         // any validation of key can go here
         //console.log("adding " + S3_PREFIX + contents[iter].Key)
         images.push(S3_PREFIX + contents[iter].Key);
@@ -39,19 +43,16 @@ function buildImagesListFromS3Data(bucketname, data) {
 //----------------------------------------------------------------------------
 router.get('/', function(req, res, next) {
 
-  // TODO pull these from ENV vars + user added buckets
-  var bucketIds = ['rhdj2017-selfie-out', 'rhdj2017-selfie-in', 'FakeBucket'];
-
-  var imagesArray = [];
-  var showBucket = req.query.showBucket;
+  let imagesArray = [];
+  let showBucket = req.query.showBucket;
   if (!showBucket) { 
     console.log('no bucket, forcing to first bucket');
-    showBucket = bucketIds[0]
+    showBucket = bucketIds[0] || fakeBucket
   }
   console.log('loading bucket: ' + showBucket);
 
   // for DEBUGGING
-  if (showBucket === 'FakeBucket') {
+  if (showBucket === fakeBucket) {
     imagesArray = ['https://s3.amazonaws.com/rhdj2017-selfie-in/59bc8771815ba5440626d40d-59bc87725d92c77569d4ddc1.png',
                  'https://s3.amazonaws.com/rhdj2017-selfie-in/59bc8771815ba5440626d40d-59bc87725d92c77569d4ddc1.png',
                  'https://s3.amazonaws.com/rhdj2017-selfie-in/59bc8771815ba5440626d40d-59bc87725d92c77569d4ddc1.png',
@@ -60,7 +61,7 @@ router.get('/', function(req, res, next) {
   } else {
     // query for images
     console.log('querying S3 for objects in ' + showBucket);
-    var params = {
+    const params = {
       Bucket: showBucket
       //ContinuationToken: 'STRING_VALUE',
       //Delimiter: 'STRING_VALUE',
