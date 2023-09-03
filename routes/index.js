7a3f3eedd0cda4635df81b59bf66cfe2111712d9
Paul Potsides <paul@koble.ai>
2023-09-03 15:17:46 +0200

    Folders and pagination.

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,6 @@ const awssecretkey = process.env.AWS_SECRET_ACCESS_KEY || '';
 const awsregion = process.env.AWS_REGION || 'us-east-1';
 
 const bucketIds = JSON.parse(process.env.BUCKETS || '[]');
-const fakeBucket = 'FakeBucket';
 const AWS = require('aws-sdk');
 if(awskey && awssecretkey && awsregion) {
   AWS.config.update({accessKeyId: awskey, secretAccessKey: awssecretkey, region: awsregion});
@@ -25,65 +24,80 @@ function filterImages(data) {
 //----------------------------------------------------------------------------
 // loop through S3 formatted API results and build an images list
 //----------------------------------------------------------------------------
-function buildImagesListFromS3Data(bucketname, data) {
+function buildFileListFromS3Data(bucketname, folder, data) {
     const S3_PREFIX = `https://${bucketname}.s3.${awsregion}.amazonaws.com/`;
-    const images = [];
-    const contents = data.Contents
-    //console.log("iterating " + JSON.stringify(contents));
-    for (const iter in contents) {
-        // any validation of key can go here
-        //console.log("adding " + S3_PREFIX + contents[iter].Key)
-        images.push(S3_PREFIX + contents[iter].Key);
+    const files = [];
+    const folders = new Set()
+    for (const iter in data) {
+        const key = data[iter].Key;
+        let prefixWithinFolder = key
+        if(folder) {
+           prefixWithinFolder = key.substring(folder.length + 1);
+        }
+        console.log({key, prefixWithinFolder})
+        if(prefixWithinFolder.indexOf("/") === -1) {
+          files.push(`${S3_PREFIX}${key}`);
+        } else {
+          const folder = prefixWithinFolder.split('/').shift();
+          console.log({folder})
+          folders.add(folder)
+        }
     }
-    return images;
+    return {files, folders: Array.from(folders)};
 }
+
+async function listObjects(bucket, folder) {
+  let contents = [];
+  let truncated = false;
+  let continuationToken = undefined;
+  do {
+    data = await s3.listObjectsV2({
+      Bucket: bucket,
+      Prefix: folder,
+      ContinuationToken: continuationToken
+    }).promise();
+    contents = contents.concat(data.Contents)
+    truncated = data.IsTruncated;
+    continuationToken = data.NextContinuationToken;
+  } while (truncated)
+  return contents;
+}
+
+router.get('/', function (_req, res, _next){
+  const bucket = bucketIds[0] || fakeBucket;
+  console.log('loading bucket: ' + bucket);
+  res.redirect(`/${bucket}/`)
+})
+
 
 //----------------------------------------------------------------------------
 // GET on the main page.
 //----------------------------------------------------------------------------
-router.get('/', function(req, res, next) {
-
-  let imagesArray = [];
-  let showBucket = req.query.showBucket;
-  if (!showBucket) { 
-    console.log('no bucket, forcing to first bucket');
-    showBucket = bucketIds[0] || fakeBucket
+router.get('/:bucket', async function(req, res, next) {
+  const { bucket } = req.params;
+  const { folder = '' } = req.query;
+  if (!bucket) { 
+    res.sendStatus(400);
+    return;
   }
-  console.log('loading bucket: ' + showBucket);
-
-  // for DEBUGGING
-  if (showBucket === fakeBucket) {
-    imagesArray = ['https://s3.amazonaws.com/rhdj2017-selfie-in/59bc8771815ba5440626d40d-59bc87725d92c77569d4ddc1.png',
-                 'https://s3.amazonaws.com/rhdj2017-selfie-in/59bc8771815ba5440626d40d-59bc87725d92c77569d4ddc1.png',
-                 'https://s3.amazonaws.com/rhdj2017-selfie-in/59bc8771815ba5440626d40d-59bc87725d92c77569d4ddc1.png',
-                 'https://s3.amazonaws.com/rhdj2017-selfie-in/59bc8771815ba5440626d40d-59bc87725d92c77569d4ddc1.png'];
-    res.render('index', { title: 'AWS S3 Image Viewer', showBucket: showBucket, images: JSON.stringify(imagesArray), buckets: JSON.stringify(bucketIds)});
-  } else {
-    // query for images
-    console.log('querying S3 for objects in ' + showBucket);
-    const params = {
-      Bucket: showBucket
-      //ContinuationToken: 'STRING_VALUE',
-      //Delimiter: 'STRING_VALUE',
-      //EncodingType: url,
-      //FetchOwner: false,
-      //MaxKeys: 50,
-      //Prefix: 'STRING_VALUE',
-      //RequestPayer: requester,
-      //StartAfter: 'STRING_VALUE'
-    };
-    s3.listObjectsV2(params, function(err, data) {
-      if (err) {
-        console.log(err, err.stack); // an error occurred
-        res.render('index', { title: 'AWS S3 Image Viewer', showBucket: showBucket, images: JSON.stringify(imagesArray), buckets: JSON.stringify(bucketIds)});
-      } else {
-        //console.log(data);
-        imagesArray = buildImagesListFromS3Data(showBucket, data);
-        filteredImagesArray = filterImages(imagesArray);
-        res.render('index', { title: 'AWS S3 Image Viewer', showBucket: showBucket, images: JSON.stringify(filteredImagesArray), buckets: JSON.stringify(bucketIds)});
-      }
-    });
+  // query for images
+  console.log('querying S3 for objects in ' + bucket);
+  try {
+    const data = await listObjects(bucket, folder);
+    const {files, folders} = buildFileListFromS3Data(bucket, folder, data);
+    console.log({files, folders})
+    filteredImagesArray = filterImages(files);
+    res.render('index', { title: 'AWS S3 Image Viewer', showBucket: bucket, images: JSON.stringify(filteredImagesArray), buckets: JSON.stringify(bucketIds), folder, folders: JSON.stringify(folders)});
+  } catch (err) {
+    console.log(err, err.stack); // an error occurred
+    res.render('index', { title: 'AWS S3 Image Viewer', showBucket: bucket, images: '[]', buckets: JSON.stringify(bucketIds)});
   }
 });
+
+
+router.get('/:bucket', function (req, res, next){
+  const bucket = req.params.bucket;
+  res.redirect(`/${bucket}/*`)
+})
 
 module.exports = router;
